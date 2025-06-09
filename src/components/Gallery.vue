26b13e23e8c44ca470564c43785569820d73cde1
S SIDHARTH <92774572+callmesidhu@users.noreply.github.com>
2025-06-09 15:25:35 +0530

    Update Gallery.vue

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -15,7 +15,7 @@
       class="mySwiper"
     >
       <!-- Dynamically generate swiper slides -->
-      <swiper-slide v-for="index in 15" :key="index">
+      <swiper-slide v-for="index in 17" :key="index">
         <img 
           :src="`/gallery/${index}.jpeg`" 
           :alt="`Gallery Image ${index}`" 
